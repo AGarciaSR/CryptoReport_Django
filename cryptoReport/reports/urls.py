bc5369b6b5d783dc7e196867845ce7410cb5bf0e
Alberto García <agarciablasco2@gmail.com>
2023-07-19 02:31:42 +0200

    Completada vista "Comisiones pagadas"
    Disponible vista que muestra las comisiones pagadas acumuladas.
    
    Se muestran ordenadas por el monto. También en la ganancia/pérdida de la vista "Volúmenes totales".

--- cryptoReport/reports/urls.py
+++ cryptoReport/reports/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('total_volumes', views.total_volumes, name='total_volumes')
+    path('total_volumes', views.total_volumes, name='total_volumes'),
+    path('fees_paid', views.fees_paid, name='fees_paid')
 ]
--- cryptoReport/reports/views.py
+++ cryptoReport/reports/views.py
@@ -5,7 +5,7 @@ from transactions.models import Coin, Exchange, Transaction
 
 # Create your views here.
 def total_volumes(request):
-    volumes = Transaction.objects.raw("SELECT 1 as id, symbol, pair_a_name_id, SUM(CASE WHEN t_type = 'buy' OR t_type = 'sell' THEN 1 ELSE 0 END) as number, pair_a_name_id, sum(CASE WHEN t_type = 'buy' THEN order_value ELSE 0 END) as volume_buy, SUM(CASE WHEN t_type = 'sell' THEN order_value ELSE 0 END) as volume_sell FROM transactions_transaction JOIN transactions_coin ON transactions_transaction.pair_a_name_id = transactions_coin.name WHERE transactions_transaction.user_id_id = "+str(request.user.id)+" GROUP BY pair_a_name_id, symbol ORDER BY symbol ASC")
+    volumes = Transaction.objects.raw("SELECT 1 as id, symbol, pair_a_name_id, SUM(CASE WHEN t_type = 'buy' OR t_type = 'sell' THEN 1 ELSE 0 END) as number, pair_a_name_id, sum(CASE WHEN t_type = 'buy' THEN order_value ELSE 0 END) as volume_buy, SUM(CASE WHEN t_type = 'sell' THEN order_value ELSE 0 END) as volume_sell, (SUM(CASE WHEN t_type = 'sell' THEN order_value ELSE 0 END) - sum(CASE WHEN t_type = 'buy' THEN order_value ELSE 0 END)) as gainloss FROM transactions_transaction JOIN transactions_coin ON transactions_transaction.pair_a_name_id = transactions_coin.name WHERE transactions_transaction.user_id_id = "+str(request.user.id)+" GROUP BY pair_a_name_id, symbol ORDER BY gainloss DESC")
     num_coins = Transaction.objects.only("pair_a_name").distinct("pair_a_name")
     for volume in volumes:
         volume.total_volume = volume.volume_sell - volume.volume_buy
@@ -16,7 +16,7 @@ def total_volumes(request):
     return render(request, "total_volumes.html", context)
 
 def fees_paid(request):
-    fees = Transaction.objects.raw("SELECT 1 as id, coin_fee_name_id, SUM(t.mount_fee) as mount, SUM(t.fee_value) as value WHERE transactions_transaction.user_id_id = "+str(request.user.id)+" AND order_value > 0 GROUP BY coin_fee_name_id ORDER BY value")
+    fees = Transaction.objects.raw("SELECT 1 as id, coin_fee, coin_fee_name_id, SUM(mount_fee) as mount, SUM(fee_value) as value FROM transactions_transaction JOIN transactions_coin ON transactions_transaction.pair_a_name_id = transactions_coin.name WHERE transactions_transaction.user_id_id = "+str(request.user.id)+" AND order_value > 0 GROUP BY coin_fee_name_id, coin_fee ORDER BY value DESC")
     context = {
         "fees": fees
     }
